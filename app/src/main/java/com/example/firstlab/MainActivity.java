d22578fac981b5eeb0c2c0941f55af8a534a52a0
NikolnikoffTheOriginal <nikolnikoffg@gmail.com>
2021-09-22 22:05:09 +0300

    Added button that will change the text

--- app/src/main/java/com/example/firstlab/MainActivity.java
+++ app/src/main/java/com/example/firstlab/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.tvMain = findViewById(R.id.tvMain);
+
     }
 
     public void onBtnClick(View view) {
